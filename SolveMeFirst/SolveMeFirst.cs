ee06732c94d4957eb8496b7e394db0d07835f8bd
camroddur99 <72636393+camroddur99@users.noreply.github.com>
2020-10-19 11:19:35 -0500

    Add files via upload

---
+++ SolveMeFirst/SolveMeFirst.cs
@@ -0,0 +1,9 @@
+using System;
+
+class SolveMeFirst{
+    static void Main(String[] args){
+        var a = int.Parse(Console.ReadLine());
+        var b = int.Parse(Console.ReadLine());
+        Console.WriteLine(a+b);
+    }
+}
